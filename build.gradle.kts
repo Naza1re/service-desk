a2fd8f8beb12b4444f41b86cb3fa47265426bc3d
Бачище Назар Сергеевич <mcarim@mail.ru>
2025-10-20 21:41:07 +0300

    SD-1: Добавлена зависимость от библиотеки entity-framework

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,8 @@ plugins {
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.5.6"
     id("io.spring.dependency-management") version "1.1.7"
+    id("maven-publish")
 }
-
-group = "com.kotlin"
-version = "0.0.1-SNAPSHOT"
-description = "service-desk"
 
 java {
     toolchain {
@@ -16,24 +13,31 @@ java {
 }
 
 repositories {
+    maven {
+        url = uri("https://maven.pkg.github.com/Naza1re/entity-framework")
+        credentials {
+            username = System.getenv("USERNAME_GITHUB")
+            password = System.getenv("TOKEN_GITHUB")            }
+    }
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter") // уже есть
-    implementation("org.springframework.boot:spring-boot-starter-aop") // уже есть
-    implementation("org.springframework.boot:spring-boot-starter-web") // уже есть
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa") // <- вот это нужно добавить
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.liquibase:liquibase-core")
-    runtimeOnly("org.postgresql:postgresql") // драйвер базы
-    implementation(files("C:\\Users\\mcari\\IdeaProjects\\aop\\build\\libs\\entity-framework-0.2.0.jar"))
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    runtimeOnly("org.postgresql:postgresql")
     implementation("com.vladmihalcea:hibernate-types-60:2.21.1")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
 
+    implementation("com.kotlin:entity-framework:0.0.1")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
