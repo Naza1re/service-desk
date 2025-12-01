852c85925f3ef8169d167422b9760eed37b1c4ca
Бачище Назар Сергеевич <mcarim@mail.ru>
2025-12-01 20:00:28 +0300

    SD-3: add 'owner' custom field

--- src/main/resources/db/changelog/migrations/data/custom-fields-to-entity-types.sql
+++ src/main/resources/db/changelog/migrations/data/custom-fields-to-entity-types.sql
@@ -5,3 +5,10 @@ SELECT
     et.id AS entity_type_id,
     true AS required
 FROM entity_type et on conflict do nothing;
+
+INSERT INTO custom_fields_entity_type (custom_field_id, entity_type_id, required)
+SELECT
+    (SELECT id FROM custom_field WHERE code = 'owner') AS custom_field_id,
+    et.id AS entity_type_id,
+    true AS required
+FROM entity_type et on conflict do nothing;
